5f01ffa106ec63784cc9c113745cf8011bc0ff7c
yamachan0625 <napoli.080625@gmail.com>
2020-03-18 19:29:59 +0900

    my first React Hook with useUtate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return <div>this is a template for React app!</div>;
+  //useStateは配列を返す[引数の内容, 関数]
+  //↓慣習的に決まっている命名規則
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  //setCountの引数には
+  const increment2 = () => setCount(previousCount => previousCount + 1);
+  const decrement2 = () => setCount(previousCount => previousCount - 1);
+
+  const resetCount = () => setCount(0);
+
+  const multipleCount = () => setCount(previousCount => previousCount * 2);
+
+  const divide3 = () =>
+    setCount(previousCount =>
+      previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    );
+
+  return (
+    <>
+      <div>count:{count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={resetCount}>Reset</button>
+        <button onClick={multipleCount}>x2</button>
+        <button onClick={divide3}>3の倍数の時３で割る</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
