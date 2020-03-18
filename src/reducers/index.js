293d79bdce02c635e3c00ff8180650f6e43655ad
yamachan0625 <napoli.080625@gmail.com>
2020-03-19 03:19:26 +0900

    implement event creation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -58,7 +58,7 @@ const events = (state = [], action) => {
     case 'CREATE_EVENT':
       const event = { title: action.title, body: action.body };
       const length = state.length;
-      const id = length === 0 ? (id = 1) : (id = state[length - 1].id + 1);
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
       // { id : id, ...event }をショートハンドで書いている
       return [...state, { id, ...event }];
 
