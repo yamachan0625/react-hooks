071dbc05723dfc4de77a9b02aa44879cd6d28518
yamachan0625 <napoli.080625@gmail.com>
2020-03-18 21:06:59 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = props => {
   //props(オブジェクト)をstateにまとめることも可能
@@ -6,24 +6,38 @@ const App = props => {
   //オブジェクト(state)を{name,price}に代入することでここに使うことができる
   const { name, price } = state;
 
+  useEffect(() => {
+    console.log(
+      'これはcomponentDidMountやcomponentDidUpdateのタイミングに似ている'
+    );
+  });
+
+  useEffect(() => {
+    console.log('これはcomponentDidMountに似ている');
+  }, []);
+
+  useEffect(() => {
+    console.log('このコールバックはname限定です');
+  }, [name]);
+
   return (
     <>
       <p>
-        現在の{name}は,{price}です
-        {/* ()...state) スプレッド構文 */}
-        {/* 一度stateを展開(...state)することでpriceのみ変更することができる */}
-        <button onClick={() => setState({ ...state, price: price + 1 })}>
-          +1
-        </button>
-        <button onClick={() => setState({ ...state, price: price - 1 })}>
-          -1
-        </button>
-        <button onClick={() => setState(props)}>Reset</button>
-        <input
-          value={name}
-          onChange={e => setState({ ...state, name: e.target.value })}
-        />
+        現在の{name}は,{price}です{renderPeriod()}
       </p>
+      {/* ()...state) スプレッド構文 */}
+      {/* 一度stateを展開(...state)することでpriceのみ変更することができる */}
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input
+        value={name}
+        onChange={e => setState({ ...state, name: e.target.value })}
+      />
     </>
   );
 };
